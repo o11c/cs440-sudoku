699e5ab5d899865af7b272f0fd10875c98f36e07
Sebastian Sanchez <sebastian.sanchez@vancouver.wsu.edu>
2013-02-19 17:08:45 -0800

    I created a new function named mostNonFilledInSector that takes as an input a state and outputs a tuple (x,y) that represent a blank sector with the most/least adjacent blank sectors to it. The second argument of the function determines the most/least adjacent blanks to the blank sector.

--- searchNxN.py
+++ searchNxN.py
@@ -2,6 +2,31 @@ import node
 from grid import *
 import itertools
 import sys, time
+from heapq import *
+
+class MyPriorityQueue:
+    """ This is priority queue that organizes objects in the queue in
+        this format ( priority value, object ). If "max" is pass as part
+        of the constructor, then the priority queue is a max-heap, otherwise,
+        it is a min-heap
+    """
+    def __init__(self,type_of_heap=""):
+        self.heap = []
+        if type_of_heap == "max":
+            self.type_ = -1
+        else:
+            self.type_ = 1
+    def put(self, weight, node):
+        heappush(self.heap, ( self.type_*weight, node ) )
+    def get(self):
+        priority, node = heappop(self.heap)
+        return node
+    def empty(self):
+        return ( len(self.heap) == 0 )
+    def __len__(self):
+        return len(self.heap)
+    def toList(self):
+        return  self.heap
 
 def search3x3(self, rootNode):
     stack = []
@@ -12,12 +37,78 @@ def search3x3(self, rootNode):
         if node.depth == node.state.nn:
             return node
         #chooseNextSector(node.state, indexX, indexY) call function to determain were to go to next
+#mostNonFilledInSector(node)
         action(node.state, indexX, indexY, move)#indexX and Y are the sector coordinates 
         if move != []:
             for n in move:
                 newNode = Node(n, node)
                 stack.append(newNode)
     return None
+
+def adjacentElement(n, secComponent):
+    """
+        This function is used by adjacentSector to find the adject x or y coordinates
+        of a single coordinate number.
+    """
+    remainder = secComponent % n
+    if remainder != 0:
+        index = range(0,secComponent) + range(secComponent+1,n)
+    else:
+        index = range(1,n)
+    return index
+
+def adjacentSector(n, secX, secY):
+    """ 
+        It creates a list of all adjacent sectors in the sector (secX,secY).
+        And it's used in chooseNextSector.
+    """
+#print (secX,secY)
+    columnTuple = zip( [ secX for x in range(0, n-1) ], adjacentElement(n, secY) )
+    rowTuple = zip( adjacentElement(n, secX) , [ secY for x in range(0, n-1) ] )
+    return rowTuple + columnTuple  
+
+def chooseNextSector(state, secX, secY):
+    n = state.n # find boundries
+    adjacentSector_ = adjacentSector(n, secX, secY)
+    blankSector = []
+
+    for x,y in adjacentSector_: # Getting rid of filled sectors
+        if not isFilled(state, x, y):
+            blankSector.append( (x,y) )
+
+#print (secX,secY)
+    return blankSector
+def nonFilledInSector(state):
+    n = state.n # find boundries
+    allSectors = [( x, y ) for y in range(0,n) for x in range(0,n)]
+
+    blankSector = []
+    for x,y in allSectors: # Getting rid of filled sectors
+        if not isFilled(state, x, y):
+            blankSector.append( (x,y) )
+    return blankSector
+
+def mostNonFilledInSector(state, sortby="max"):
+    """
+        It returns a tuple of a blank sector with the least/most
+        number of adjacent blank sectors to it.
+
+        To return tuple with least adjacent number of blanks, "min" has
+        to be passed as the second argument. Otherwise, it returns the 
+        one with the most blanks.
+    """
+    availableSector = []
+    availableSector = nonFilledInSector(state)
+    if availableSector != []:
+                myPriQueue = MyPriorityQueue(sortby)
+                for sector in availableSector:
+                    x,y = sector
+                    xy_sectors = chooseNextSector(state, x, y)
+                    myPriQueue.put( len( xy_sectors ) , (x,y))
+#print myPriQueue.toList()
+                return myPriQueue.get()
+    else:
+        return []
 
 def action(state, secX, secY, list1):
     """Given a node and sector indecies this function will return a list of
@@ -46,7 +137,7 @@ def action(state, secX, secY, list1):
                 return
 
 def isFilled(state, secX, secY):
-    """Given a state and sector cordinates return ture if sector is filled in,
+    """Given a state and sector cordinates return true if sector is filled in,
        else return false."""
 
     return (Cell(0) not in reduce(operator.add, state.square(secX, secY)))
@@ -140,7 +231,7 @@ if __name__ == "__main__":
     board = ".94...13..........3...76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
              
     sudokuBoard = Grid(3, board)
-    print sudokuBoard, '\n'
+#print sudokuBoard, '\n'
 
     """
         x = column
@@ -159,8 +250,6 @@ if __name__ == "__main__":
 
     x = []
     t = time.clock()
-    action(sudokuBoard, 0, 0, x)
+
     t2 = time.clock()
-    for y in x:
-        print y, '\n'
     print t2 - t, '\n'
